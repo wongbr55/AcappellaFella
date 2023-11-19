e6a03f3c3ee5073f9d5fd0d8708ce718e1f1c081
markestiller <markalestiller@gmail.com>
2023-11-19 00:50:38 -0500

    Integrated scoreboard feature with player guess

--- src/app/PlayerGuessViewBuilder.java
+++ src/app/PlayerGuessViewBuilder.java
@@ -3,13 +3,14 @@ package app;
 import interface_adapter.PlayerGuess.PlayerGuessViewModel;
 import view.ChatView;
 import view.PlayerGuessView;
+import view.ScoreboardView;
 
 public class PlayerGuessViewBuilder {
     private PlayerGuessViewBuilder() {
     }
 
-    public static PlayerGuessView createView(ChatView chatView, PlayerGuessViewModel playerGuessViewModel) {
-        return new PlayerGuessView(chatView, playerGuessViewModel);
+    public static PlayerGuessView createView(ScoreboardView scoreboardView, ChatView chatView, PlayerGuessViewModel playerGuessViewModel) {
+        return new PlayerGuessView(scoreboardView, chatView, playerGuessViewModel);
     }
 
 }
