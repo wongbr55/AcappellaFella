26304fd54a4c2398f44a4f85b23fd08b44d93767
markestiller <markalestiller@gmail.com>
2023-11-20 14:21:48 -0500

    Implemented AddPlayerController

--- src/interface_adapter/AddPlayer/AddPlayerController.java
+++ src/interface_adapter/AddPlayer/AddPlayerController.java
@@ -1,4 +1,18 @@
 package interface_adapter.AddPlayer;
 
+import entity.Player;
+import use_case.AddPlayer.AddPlayerInputBoundary;
+import use_case.AddPlayer.AddPlayerInputData;
+
 public class AddPlayerController {
+    private final AddPlayerInputBoundary addPlayerInteractor;
+
+    public AddPlayerController(AddPlayerInputBoundary addPlayerInteractor) {
+        this.addPlayerInteractor = addPlayerInteractor;
+    }
+
+    public void execute(Player player) {
+        AddPlayerInputData addPlayerInputData = new AddPlayerInputData(player);
+        addPlayerInteractor.execute(addPlayerInputData);
+    }
 }
