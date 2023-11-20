3b2563b76430e85c9028c77b4b3ffdb02689e8a2
Brandon <sudburymailing@gmail.com>
2023-11-20 14:03:01 -0500

    Added skeleton files for the AddPlayer use case

---
+++ src/interface_adapter/AddPlayer/AddPlayerController.java
@@ -0,0 +1,4 @@
+package interface_adapter.AddPlayer;
+
+public class AddPlayerController {
+}
---
+++ src/use_case/AddPlayer/AddPlayerPlayerDataAccessInterface.java
@@ -0,0 +1,4 @@
+package use_case.AddPlayer;
+
+public interface AddPlayerPlayerDataAccessInterface {
+}
