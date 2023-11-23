17f28640afbc494e43385da46813cd1958edc02a
Eric Liang <eliangca@outlook.com>
2023-11-22 20:22:22 -0500

    Fixed bug where guessStatus wouldn't update for other players

--- src/use_case/ReceiveMessage/ReceiveMessageInteractor.java
+++ src/use_case/ReceiveMessage/ReceiveMessageInteractor.java
@@ -1,6 +1,8 @@
 package use_case.ReceiveMessage;
 
 import entity.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReceiveMessageInteractor implements ReceiveMessageInputBoundary {
     final ReceiveMessageGameStateDataAccessInterface gameStateDataAccessObject;
@@ -32,10 +34,39 @@ public class ReceiveMessageInteractor implements ReceiveMessageInputBoundary {
         GameState gameState = gameStateDataAccessObject.getGameState();
         RoundState roundState = roundStataDataAccessObject.getCurrentRoundState();
 
+        String patternString = "(.+?) has guessed the answer!";
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(content);
+
+        // if it matches
+        if (type == Message.MessageType.SYSTEM && matcher.matches()) {
+            String playerName = matcher.group(1);
+
+            // todo more updateScore use case
+            player.setScore(gameState.getMainPlayer().getScore() + 1);
+            roundState.setGuessStatusByPlayer(player, true);
+        }
+
         // don't show the message if the player hasn't guess it yet and it comes from a player who has guessed it
         boolean showMessage = type != Message.MessageType.GUESSED || roundState.getGuessStatusByPlayer(gameState.getMainPlayer());
 
         ReceiveMessageOutputData receiveMessageOutputData = new ReceiveMessageOutputData(message, showMessage);
         receiveMessagePresenter.prepareSuccessView(receiveMessageOutputData);
+    }
+
+    public static void main(String[] args) {
+        String patternString = "SYSTEM\\nSYSTEM\\n(.+?) has guessed the answer!";
+        String inputString = "SYSTEM\nSYSTEM\nWAhasdzmaksdl asldkjaskldjalksd has guessed the answer!";
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(inputString);
+
+        if (matcher.matches()) {
+            String playerName = matcher.group(1);
+            System.out.println("Player Name: " + playerName);
+        } else {
+            System.out.println("String does not match the pattern");
+        }
     }
 }
