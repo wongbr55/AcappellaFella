358228d7c1fe44dac8061983438c652daee4c174
Brandon Wong <sudburymailing@gmail.com>
2023-11-27 17:31:03 -0500

    Implemented requirements for EndScreenState and ViewModel

--- src/interface_adapter/EndScreen/EndScreenState.java
+++ src/interface_adapter/EndScreen/EndScreenState.java
@@ -1,4 +1,19 @@
 package interface_adapter.EndScreen;
 
 public class EndScreenState {
+    String firstPlayer;
+    Integer firstScore;
+
+    public EndScreenState(){}
+
+    public void setFirstPlace(String firstPlayer, Integer firstScore){
+        this.firstScore = firstScore;
+        this.firstPlayer = firstPlayer;
+    }
+    public Integer getFirstScore() {
+        return firstScore;
+    }
+    public String getFirstPlayer() {
+        return firstPlayer;
+    }
 }
--- src/view/EndScreenView.java
+++ src/view/EndScreenView.java
@@ -1,4 +1,33 @@
 package view;
 
-public class EndScreenView {
+import interface_adapter.EndScreen.EndScreenViewModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class EndScreenView extends JPanel implements ActionListener, PropertyChangeListener {
+
+    private JLabel firstPlayerName;
+
+    private JButton playAgain;
+    private JButton toMenu;
+
+    public EndScreenView(EndScreenViewModel endScreenViewModel){
+        endScreenViewModel.addPropertyChangeListener(this);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
