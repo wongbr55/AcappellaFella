60cc67759a826922e76997b79d74fc75b7e1f546
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-29 18:38:12 -0500

    StartLobbyOutputBoundary implemented

--- src/use_case/StartLobby/StartLobbyOutputBoundary.java
+++ src/use_case/StartLobby/StartLobbyOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.StartLobby;
 
 public interface StartLobbyOutputBoundary {
+    void prepareSuccessView(StartLobbyOutputData startLobbyOutputData);
+
+    void prepareFailView(String error);
 }
