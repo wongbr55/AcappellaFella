016814c9b76d025338affc23a1fece6c62e98113
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-29 18:47:38 -0500

    StartLobbyInteractor implemented

--- src/use_case/StartLobby/StartLobbyInteractor.java
+++ src/use_case/StartLobby/StartLobbyInteractor.java
@@ -1,4 +1,36 @@
 package use_case.StartLobby;
 
-public class StartLobbyInteractor {
+import entity.Player;
+
+public class StartLobbyInteractor implements StartLobbyOutputBoundary {
+    final StartLobbyDataAccessInterface startLobbyDataAccessInterface;
+    final StartLobbyOutputBoundary startLobbyOutputBoundary;
+
+    public StartLobbyInteractor(StartLobbyDataAccessInterface startLobbyDataAccessInterface, StartLobbyOutputBoundary startLobbyOutputBoundary){
+        this.startLobbyDataAccessInterface = startLobbyDataAccessInterface;
+        this.startLobbyOutputBoundary = startLobbyOutputBoundary;
+    }
+
+    public void execute(StartLobbyInputData startLobbyInputData){
+        if (startLobbyDataAccessInterface.existsByName(startLobbyInputData.getName())){
+            startLobbyOutputBoundary.prepareFailView("Name already in use");
+        }else {
+            Player player = new Player();
+            player.setName(startLobbyInputData.getName());
+            startLobbyDataAccessInterface.save(player);
+
+            StartLobbyOutputData startLobbyOutputData = new StartLobbyOutputData(player.getName());
+            startLobbyOutputBoundary.prepareSuccessView(startLobbyOutputData);
+        }
+    }
+
+    @Override
+    public void prepareSuccessView(StartLobbyOutputData startLobbyOutputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
