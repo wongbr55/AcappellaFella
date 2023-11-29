c14f010b0fe4e4da93702870669b48864bfc0138
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-29 18:37:17 -0500

    StartLobbyInputData implemented

--- src/use_case/StartLobby/StartLobbyInputData.java
+++ src/use_case/StartLobby/StartLobbyInputData.java
@@ -1,4 +1,11 @@
 package use_case.StartLobby;
 
 public class StartLobbyInputData {
+    final private String name;
+
+    public StartLobbyInputData(String name){
+        this.name = name;
+    }
+
+    String getName(){return name;}
 }
