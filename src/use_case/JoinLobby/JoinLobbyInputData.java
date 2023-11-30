87b6b211dadfd900255724d6931d4b36c71654b7
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-30 01:07:41 -0500

    JoinLobbyInputData implemented

--- src/use_case/JoinLobby/JoinLobbyInputData.java
+++ src/use_case/JoinLobby/JoinLobbyInputData.java
@@ -1,4 +1,11 @@
 package use_case.JoinLobby;
 
 public class JoinLobbyInputData {
+    final private String name;
+
+    public JoinLobbyInputData(String name){
+        this.name = name;
+    }
+
+    String getName(){return name;}
 }
