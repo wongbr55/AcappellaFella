6b885d51df4155a82453ce7b836637c468193560
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-30 01:08:32 -0500

    JoinLobbyOutputBoundary implemented

--- src/use_case/JoinLobby/JoinLobbyOutputBoundary.java
+++ src/use_case/JoinLobby/JoinLobbyOutputBoundary.java
@@ -1,4 +1,7 @@
 package use_case.JoinLobby;
 
 public interface JoinLobbyOutputBoundary {
+    void prepareSuccessView(JoinLobbyOutputData joinLobbyOutputData);
+
+    void prepareFailView(String error);
 }
