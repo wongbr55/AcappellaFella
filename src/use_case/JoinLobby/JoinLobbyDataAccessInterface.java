cc635df7c8a2bedb8a03221ccdd4de69e48235e6
Shiyan <shiyan.khandakar@mail.utoronto.ca>
2023-11-30 01:08:51 -0500

    JoinLobbyDataAccessInterface implemented

--- src/use_case/JoinLobby/JoinLobbyDataAccessInterface.java
+++ src/use_case/JoinLobby/JoinLobbyDataAccessInterface.java
@@ -1,4 +1,9 @@
 package use_case.JoinLobby;
 
+import entity.Player;
+
 public interface JoinLobbyDataAccessInterface {
+    boolean existsByName(String identifier);
+
+    void save(Player player);
 }
