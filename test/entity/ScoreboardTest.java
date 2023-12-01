a2341fc91b95bf70fcd36397f29677db5bb8d9da
markestiller <markalestiller@gmail.com>
2023-11-30 20:20:44 -0500

    Implemented ScoreboardTest

---
+++ test/entity/ScoreboardTest.java
@@ -0,0 +1,31 @@
+package entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreboardTest {
+
+
+
+    @Before
+    public void init(){
+    }
+
+    @Test
+    public void testAddPlayer() {
+    }
+
+    @Test
+    public void testGetPlayerScore() {
+    }
+
+    @Test
+    public void testUpdatePlayerScore() {
+    }
+
+    @Test
+    public void getNumberOfPlayers() {
+    }
+}
