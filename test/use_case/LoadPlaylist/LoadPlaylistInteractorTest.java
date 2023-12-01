b8f600181cf541f767828453977b128a1f848a66
markestiller <markalestiller@gmail.com>
2023-11-30 23:48:03 -0500

    Implemented LoadPlaylistInteractorTest, check .env problem

---
+++ test/use_case/LoadPlaylist/LoadPlaylistInteractorTest.java
@@ -0,0 +1,30 @@
+package use_case.LoadPlaylist;
+
+import data_access.APIDataAccessObject;
+import entity.Playlist;
+import entity.Song;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoadPlaylistInteractorTest {
+
+    private APIDataAccessObject loadPlaylistDataAccessObject;
+
+    private LoadPlaylistInteractor loadPlaylistInteractor;
+    @Before
+    public void init() {
+        this.loadPlaylistDataAccessObject = new APIDataAccessObject();
+        this.loadPlaylistInteractor = new LoadPlaylistInteractor(this.loadPlaylistDataAccessObject);
+    }
+
+    @Test
+    public void execute() {
+        Playlist playlist = loadPlaylistInteractor.execute();
+        String songOne = playlist.getSongOne().toString();
+        String songTwo = playlist.getSongTwo().toString();
+        String songThree = playlist.getSongThree().toString();
+        assertTrue(!songOne.equals(songTwo) && !songOne.equals(songThree) && !songTwo.equals(songThree));
+    }
+}
