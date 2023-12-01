3d5e8143ff03a0f019250578a1fbe746bf1ae7b3
markestiller <markalestiller@gmail.com>
2023-12-01 00:42:01 -0500

    Progress on UpdateScoreInteractorTest, need to fix problems with presenter

---
+++ test/use_case/UpdateScore/UpdateScoreInteractorTest.java
@@ -0,0 +1,54 @@
+package use_case.UpdateScore;
+
+import data_access.InMemoryRoundStateDataAccessObject;
+import data_access.InMemoryScoreboardScoreboardDataAccessObject;
+import entity.Player;
+import entity.RoundState;
+import entity.Scoreboard;
+import interface_adapter.UpdateScore.UpdateScorePresenter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UpdateScoreInteractorTest {
+
+    private InMemoryRoundStateDataAccessObject roundStateDataAccessObject;
+    private InMemoryScoreboardScoreboardDataAccessObject scoreboardScoreboardDataAccessObject;
+
+    private UpdateScoreInteractor updateScoreInteractor;
+
+    private UpdateScorePresenter updateScorePresenter;
+    @Before
+    public void init() {
+        this.roundStateDataAccessObject = new InMemoryRoundStateDataAccessObject();;
+        this.scoreboardScoreboardDataAccessObject = new InMemoryScoreboardScoreboardDataAccessObject();
+        this.updateScoreInteractor = new UpdateScoreInteractor(scoreboardScoreboardDataAccessObject, roundStateDataAccessObject, updateScorePresenter);
+    }
+
+    @Test
+    public void execute() {
+        // Instantiate two player, score = 0 by default
+        Player playerA = new Player();
+        playerA.setName("Mark");
+        Player playerB = new Player();
+        playerB.setName("Bark");
+
+        // Add player to Scoreboard
+        Scoreboard scoreboard = scoreboardScoreboardDataAccessObject.getScoreboard();
+        scoreboard.addPlayer(playerA);
+        scoreboard.addPlayer(playerB);
+
+        // player A guesses right
+        roundStateDataAccessObject.addRound();
+        RoundState roundState = roundStateDataAccessObject.getCurrentRoundState();
+        roundState.setGuessStatusByPlayer(playerA, true);
+        roundState.setGuessStatusByPlayer(playerB, false);
+
+        // execute
+        UpdateScoreInputData updateScoreInputData = new UpdateScoreInputData(playerA);
+        this.updateScoreInteractor.execute(updateScoreInputData);
+        int newScore = this.scoreboardScoreboardDataAccessObject.getScoreboard().getPlayerScore(playerA);
+        assertEquals(1, newScore);
+    }
+}
