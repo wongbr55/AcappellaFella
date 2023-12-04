ab26e81c3ef4bb904029684b0cab69b7a48fd95a
Brandon Wong <sudburymailing@gmail.com>
2023-12-04 16:54:00 -0500

    Removed buttons in EndView. Decided with the team due to the sake of time not to implement

--- src/view/EndScreenView.java
+++ src/view/EndScreenView.java
@@ -15,42 +15,16 @@ public class EndScreenView extends JPanel implements ActionListener, PropertyCha
     private final EndScreenViewModel endScreenViewModel;
     public final String viewName;
 
-    public EndScreenView(EndScreenViewModel endScreenViewModel){
+    public EndScreenView(EndScreenViewModel endScreenViewModel) {
         this.endScreenViewModel = endScreenViewModel;
         this.viewName = this.endScreenViewModel.getViewName();
         this.endScreenViewModel.addPropertyChangeListener(this);
         EndScreenState state = this.endScreenViewModel.getState();
         this.firstPlayer = new JLabel("Game Over! " + state.getFirstPlayerName() + " won with a final score of "
-        + state.getFirstScore() + " points!");
+                + state.getFirstScore() + " points!");
 
-        JButton playAgain = new JButton("Play Again");
-        JButton toMenu = new JButton("Back To Menu");
 
         this.add(firstPlayer);
-        JPanel buttons = new JPanel();
-        buttons.add(playAgain);
-        buttons.add(toMenu);
-        this.add(buttons);
-
-        playAgain.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(playAgain)) {
-                            // todo add relevant methods here, call to RunGame
-                        }
-                    }
-                }
-        );
-        toMenu.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent evt) {
-                        if (evt.getSource().equals(toMenu)) {
-                            // todo add relevant methods here, call to HomeView
-                        }
-                    }
-                }
-        );
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
     }
 
