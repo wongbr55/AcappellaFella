3f4d9c7d6fa58ae413aa6e716538dccd07349352
Brandon Wong <sudburymailing@gmail.com>
2023-12-05 16:43:24 -0500

    Implemented HostEnterWaitRoomInteractorTest

---
+++ test/use_case/EnterWaitRoom/HostEnterWaitRoom/HostEnterWaitRoomInteractorTest.java
@@ -0,0 +1,35 @@
+package use_case.EnterWaitRoom.HostEnterWaitRoom;
+
+import interface_adapter.EnterWaitRoom.HostEnterWaitRoom.HostEnterWaitRoomController;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HostEnterWaitRoomInteractorTest {
+
+    private HostEnterWaitRoomInteractor hostEnterWaitRoomInteractor;
+    @Before
+    public void setUp() {
+        HostEnterWaitRoomOutputBoundary hostEnterWaitRoomOutputBoundary = new HostEnterWaitRoomOutputBoundary() {
+            @Override
+            public void prepareSuccessView(HostEnterWaitRoomOutputData hostEnterWaitRoomOutputData) {
+
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+
+            }
+        };
+
+        hostEnterWaitRoomInteractor = new HostEnterWaitRoomInteractor(hostEnterWaitRoomOutputBoundary);
+    }
+
+    @Test
+    public void execute() {
+        HostEnterWaitRoomInputData hostEnterWaitRoomInputData = new HostEnterWaitRoomInputData("123", "Brandon");
+        hostEnterWaitRoomInteractor.execute(hostEnterWaitRoomInputData);
+
+    }
+}
